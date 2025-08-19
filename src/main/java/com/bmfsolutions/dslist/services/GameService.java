987f83bc9687613da2b28c6b68511bf92a6d999a
Adão Vieira <adaobmf79@gmail.com>
2025-08-19 20:27:15 -0300

    feat: add find game by id endpoint

--- src/main/java/com/bmfsolutions/dslist/services/GameService.java
+++ src/main/java/com/bmfsolutions/dslist/services/GameService.java
@@ -1,11 +1,14 @@
 package com.bmfsolutions.dslist.services;
 
 import java.util.List;
+
+import com.bmfsolutions.dslist.dto.GameDTO;
 import com.bmfsolutions.dslist.dto.GameMinDTO;
 import com.bmfsolutions.dslist.entities.Game;
 import com.bmfsolutions.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
@@ -13,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game game =  gameRepository.findById(id).get();
+        return new GameDTO(game);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
